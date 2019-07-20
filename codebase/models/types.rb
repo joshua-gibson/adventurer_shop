71fb0b54e0439c17b165a586e78dc87efe45aaf9
Joshua Gibson <flash50k@gmail.com>
2019-07-20 13:32:10 +0100

    Type.save method working

--- codebase/models/types.rb
+++ codebase/models/types.rb
@@ -1,13 +1,39 @@
+require_relative("../db/sql_runner.rb")
+
 class Type
 
-attr_reader :id
-attr_accessor :category, :usable_by
+  attr_reader :id
+  attr_accessor :name, :category, :usable_by
 
-def initialize(options)
-  @id = options['id'].to_i if options[:id]
-  @category = options['category']
-  @usable_by = options['usable_by']
-end
+  def initialize(options)
+    @id = options['id'].to_i if options[:id]
+    @name = options['name']
+    @category = options['category']
+    @usable_by = options['usable_by']
+  end
 
+
+  def save()
+    sql="INSERT INTO types (name, category, usable_by) VALUES ($1, $2, $3) RETURNING id"
+    values=[@name, @category, @usable_by]
+    @id = SqlRunner.run(sql,values).first['id'].to_i
+  end
+
+
+  #
+  #
+  # def update()
+  #
+  # end
+  #
+  #
+  # def delete()
+  #
+  # end
+  #
+  #
+  # self.delete_all()
+  #
+  # end
 
 end
--- codebase/specs/test_types.rb
+++ codebase/specs/test_types.rb
@@ -6,11 +6,12 @@ require_relative('../models/types.rb')
 
 class DrinkTest < MiniTest::Test
 
-def test_types_attrs()
-  newtype = Type.new({'category' => 'Staff', 'usable_by' => 'Mage'})
-   assert_equal('Staff', newtype.category)
-   assert_equal('Mage', newtype.usable_by)
-end
+  def test_types_attrs()
+    newtype = Type.new({'name' => 'Staff', 'category' => 'Weapon', 'usable_by' => 'Mage'})
+     assert_equal('Staff', newtype.name)
+     assert_equal('Weapon', newtype.category)
+     assert_equal('Mage', newtype.usable_by)
+  end
 
 
 
