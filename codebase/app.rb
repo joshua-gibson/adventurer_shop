1724353643308481e7997d851cd6c6364d28ae4b
Joshua Gibson <flash50k@gmail.com>
2019-07-22 15:54:01 +0100

    moved controllers to separate folder

--- codebase/app.rb
+++ codebase/app.rb
@@ -6,8 +6,8 @@ require_relative('./models/items.rb')
 require_relative('./models/types.rb')
 require_relative('./models/categories.rb')
 
-require_relative('./shop_controller.rb')
-require_relative('./type_controller.rb')
-require_relative('./category_controller.rb')
+require_relative('./controllers/shop_controller.rb')
+require_relative('./controllers/type_controller.rb')
+require_relative('./controllers/category_controller.rb')
 
 also_reload('./models/*')
