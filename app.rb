4c1eb776af905dfdbf8feccf3b4fb7f0558ea24e
Joshua Gibson <flash50k@gmail.com>
2020-05-12 15:14:37 +0100

    changed the folder structure to be more sensible.

--- app.rb
+++ app.rb
@@ -8,18 +8,18 @@ require( 'sinatra' )
 
 class App < Sinatra::Base
 
-require_relative('./codebase/models/items.rb')
-require_relative('./codebase/models/types.rb')
-require_relative('./codebase/models/categories.rb')
-require_relative('./codebase/models/character_classes.rb')
-require_relative('./codebase/models/ctrel.rb')
+require_relative('./models/items.rb')
+require_relative('./models/types.rb')
+require_relative('./models/categories.rb')
+require_relative('./models/character_classes.rb')
+require_relative('./models/ctrel.rb')
 
-require_relative('./codebase/controllers/shop_controller.rb')
-require_relative('./codebase/controllers/type_controller.rb')
-require_relative('./codebase/controllers/category_controller.rb')
-require_relative('./codebase/controllers/cclasses_controller.rb')
-require_relative('./codebase/controllers/ctrel_controller.rb')
+require_relative('./controllers/shop_controller.rb')
+require_relative('./controllers/type_controller.rb')
+require_relative('./controllers/category_controller.rb')
+require_relative('./controllers/cclasses_controller.rb')
+require_relative('./controllers/ctrel_controller.rb')
 
-# also_reload('./codebase/models/*')
+# also_reload('./models/*')
 
 end
